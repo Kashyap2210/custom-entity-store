211db71c133b3bf10568c099038b1c8183833d13
Kashyap Solanki <kash.cdac@gmail.com>
2026-03-27 14:05:07 +0530

    unused-code-remove

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 export * from "./app-root-store";
 export * from "./register-store";
-// export * from "./useEntityData";
 export * from "./useStore";
 export * from "./root-store";
 export * from "./base-store";
-// export * from "./useRootStore";
-// export * from "./useBaseStore";
-// export * from "./useAppRootStore";
